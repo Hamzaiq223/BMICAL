f7552f5f04f64ab9b0d47c0ae7084d71cfbfc346
mac <mac@macs-mbp-2.home>
2023-09-02 23:30:05 +0500

    Dialogs Added Successfully

--- app/src/main/java/com/example/bmicalculator/BMICalculator.java
+++ app/src/main/java/com/example/bmicalculator/BMICalculator.java
@@ -8,6 +8,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -35,10 +36,12 @@ public class BMICalculator extends AppCompatActivity  implements View.OnClickLis
     NumberPicker feetPicker, inchPicker;
     int feetValue = 5 , inchValue = 4;
     Button calculateBtn;
-    String heightValue;
+    String heightValue,heightDialog = "CM",weightDialog = "KG";
     DecimalFormat decimalFormat;
 
     private AdView adView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,25 +85,14 @@ public class BMICalculator extends AppCompatActivity  implements View.OnClickLis
         tvKG.setOnClickListener(this);
         tvLBS.setOnClickListener(this);
         height_counter_text.setOnClickListener(this);
+        weightCounterText.setOnClickListener(this);
 
-
-//        feetPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//            @Override
-//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//                feetValue = newVal;
-//                heightValueIs();
-//
-//            }
-//        });
-//
-//        inchPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//            @Override
-//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//                inchValue = newVal;
-//                heightValueIs();
-//            }
-//        });
-        calculateBtn.setOnClickListener(v -> calculateBmi());
+        calculateBtn.setOnClickListener(view -> {
+            if(weightDialog.equals("KG"))
+            {
+                calculateBmi();
+            }
+        });
 
     }
 
@@ -125,6 +117,12 @@ public class BMICalculator extends AppCompatActivity  implements View.OnClickLis
                 break;
             case R.id.age_cardView:
                 break;
+
+            case R.id.weight_counter_text:
+                showWeightDialog(weightDialog.equals("KG") ? true : false);
+
+                break;
+
             case R.id.age_btn_inc:
                 if(ageCounter < 150)
                     ageCounter++;
@@ -140,24 +138,34 @@ public class BMICalculator extends AppCompatActivity  implements View.OnClickLis
             case R.id.tvLBS:
                 weightCounterText.setText("132");
                 weightCounter = 132;
+                weightDialog = "LBS";
                 tvLBS.setTextColor(ContextCompat.getColor(this, R.color.app_color));
-                tvKG.setTextColor(ContextCompat.getColor(this,R.color.grey));
+                tvKG.setTextColor(ContextCompat.getColor(this,R.color.dark_grey));
                 break;
             case R.id.tvCM:
                 height_counter_text.setText("152");
+                heightDialog = "CM";
+                tvCM.setTextColor(ContextCompat.getColor(this, R.color.app_color));
+                tvFeet.setTextColor(ContextCompat.getColor(this,R.color.dark_grey));
                 break;
             case R.id.tvFeet:
+                height_counter_text.setText("5.0");
+                heightDialog = "FEET";
+                tvFeet.setTextColor(ContextCompat.getColor(this, R.color.app_color));
+                tvCM.setTextColor(ContextCompat.getColor(this,R.color.dark_grey));
                 break;
             case R.id.tvKG:
                 weightCounterText.setText("50");
                 weightCounter = 50;
-                tvLBS.setTextColor(ContextCompat.getColor(this, R.color.grey));
+                weightDialog = "KG";
+                tvLBS.setTextColor(ContextCompat.getColor(this, R.color.dark_grey));
                 tvKG.setTextColor(ContextCompat.getColor(this,R.color.app_color));
                 break;
 
             case R.id.height_counter_text:
-                showCenteredDialog();
+                showCenteredDialog(heightDialog.equals("CM") ? true : false);
                 break;
+
 
         }
     }
@@ -190,25 +198,113 @@ public class BMICalculator extends AppCompatActivity  implements View.OnClickLis
         intent.putExtra("bmiVal",bmiValue);
         startActivity(intent);
     }
-    private void showCenteredDialog() {
+    private void showCenteredDialog(Boolean check) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
         View dialogView = inflater.inflate(R.layout.centered_dialog, null);
         builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
 
+        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
+        Button btnOk = dialogView.findViewById(R.id.btnOK);
+        TextView tvAverageHeight = dialogView.findViewById(R.id.tvAverageHeight);
+        CardView cvCM = dialogView.findViewById(R.id.cm_cardView);
+        CardView cvFeet = dialogView.findViewById(R.id.feet_cardView);
+        CardView cvInch = dialogView.findViewById(R.id.inch_cardView);
+        NumberPicker cmPicker = dialogView.findViewById(R.id.cm_picker);
         feetPicker = dialogView.findViewById(R.id.feet_picker);
         inchPicker = dialogView.findViewById(R.id.inch_picker);
         height_title_text = dialogView.findViewById(R.id.height_title_text);
 
-        feetPicker.setMinValue(1);
-        feetPicker.setMaxValue(8);
-        inchPicker.setMinValue(0);
-        inchPicker.setMaxValue(11);
-        feetPicker.setValue(5);
-        inchPicker.setValue(4);
+        tvAverageHeight.setText(check ? "Average Height : 152 cm" : "Average Height : 5.0 ft");
+        cvFeet.setVisibility(check ? View.GONE : View.VISIBLE);
+        cvInch.setVisibility(check ? View.GONE : View.VISIBLE);
+        cvCM.setVisibility(check ? View.VISIBLE : View.GONE);
 
 
+        btnOk.setOnClickListener(view -> {
+             dialog.dismiss();
+             height_counter_text.setText(check ? String.format("%s", cmPicker.getValue()) : String.format("%s.%s", feetPicker.getValue(), inchPicker.getValue()));
+        });
+
+        btnCancel.setOnClickListener(view -> {
+             dialog.dismiss();
+        });
+
+
+        if(check){
+            cmPicker.setMinValue(30);
+            cmPicker.setMaxValue(300);
+            cmPicker.setValue(Integer.parseInt(height_counter_text.getText().toString()));
+        }
+        else{
+
+            String input = height_counter_text.getText().toString();
+            String[] parts = input.split("\\.");
+
+            feetPicker.setMinValue(1);
+            feetPicker.setMaxValue(8);
+            inchPicker.setMinValue(0);
+            inchPicker.setMaxValue(11);
+
+            int ft = Integer.parseInt(parts[0]);
+            int inch = Integer.parseInt(parts[1]);
+            feetPicker.setValue(ft);
+            inchPicker.setValue(inch);
+
+        }
+
+        dialog.show();
+    }
+
+    private void showWeightDialog(Boolean check) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.weight_dialog, null);
+        builder.setView(dialogView);
         AlertDialog dialog = builder.create();
+
+        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
+        Button btnOk = dialogView.findViewById(R.id.btnOK);
+        TextView tvAverageHeight = dialogView.findViewById(R.id.tvAverageHeight);
+
+        CardView cvKG = dialogView.findViewById(R.id.kg_cardView);
+        CardView cvLbs = dialogView.findViewById(R.id.lbs_cardView);
+        NumberPicker kgPicker = dialogView.findViewById(R.id.kg_picker);
+        NumberPicker lbsPicker = dialogView.findViewById(R.id.lbs_picker);
+        height_title_text = dialogView.findViewById(R.id.height_title_text);
+
+        tvAverageHeight.setText(check ? "Average Weight : 60 kg" : "Average Weight : 132 lbs");
+
+        cvKG.setVisibility(check ? View.VISIBLE : View.GONE);
+        cvLbs.setVisibility(check ? View.GONE : View.VISIBLE);
+
+        btnOk.setOnClickListener(view -> {
+            dialog.dismiss();
+            weightCounterText.setText(check ? String.format("%s", kgPicker.getValue()) : String.format("%s", lbsPicker.getValue()));
+            weightCounter = check ? kgPicker.getValue() : lbsPicker.getValue();
+
+        });
+
+        btnCancel.setOnClickListener(view -> {
+            dialog.dismiss();
+        });
+
+
+        if(check){
+            kgPicker.setMinValue(10);
+            kgPicker.setMaxValue(300);
+            kgPicker.setValue(Integer.parseInt(weightCounterText.getText().toString()));
+        }
+        else{
+
+            lbsPicker.setMinValue(22);
+            lbsPicker.setMaxValue(660);
+
+            lbsPicker.setValue(Integer.parseInt(weightCounterText.getText().toString()));
+
+        }
+
         dialog.show();
     }
 }
